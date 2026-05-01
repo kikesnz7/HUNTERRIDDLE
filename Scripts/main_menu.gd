c9843122c004d28dbab7b1dfce2d970001f2e220
Azabache23 <gzubache@gmail.com>
2026-05-01 16:48:17 +0200

    Ajustes en las interfaces de las escenas

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_entrar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas/Menu.tscn")
+	get_tree().change_scene_to_file("res://Escenas/LoginScreen.tscn")
 
 func _on_config_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/Config.tscn")
