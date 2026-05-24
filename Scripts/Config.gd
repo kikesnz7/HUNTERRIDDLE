7647b707e9d52cd8e40257e72df7bc276ceeb11a
Azabache23 <gzubache@gmail.com>
2026-05-24 18:18:17 +0200

    Nueva Ventana de Puzzles y assets flecha y boton

--- Scripts/Config.gd
+++ Scripts/Config.gd
@@ -4,7 +4,9 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func _on_volver_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/StartMenu.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
